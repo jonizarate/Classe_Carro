2761ed7b27c3ee72a93085b81d815a1e311b6bab
jonizarate <jonizarate00@gmail.com>
2022-10-28 20:52:42 -0300

    Agregar archivos de proyecto.

---
+++ Carro.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe_Carro
+{
+    public class Carro
+    {
+        public string modelo { get; set; }
+        public string marca { get; set; }
+        public int anio { get; set; }
+        public string tipo { get; set; }
+        public string color { get; set; }
+        public double cilindrada { get; set; }
+        public int numeroDeSerie { get; set; }
+        public int canEjes { get; set; }
+        public bool  andando {get;set;}
+        public bool bocina { get; set; }
+
+        public  Carro()
+        {
+            modelo ="";
+            marca = "";
+            anio =0;
+            tipo = "";
+            color = "";
+            cilindrada = 0;
+            numeroDeSerie=0;
+            canEjes =0;
+            andando =false;
+            bocina = false;
+        }
+
+        public Carro(string modelo, string marca, int anio, string tipo, string color, double cilindrada, int numeroDeSerie, int canEjes, bool andando,bool bocina)
+        {
+            this.modelo = modelo;
+            this.marca = marca;
+            this.anio = anio;
+            this.tipo = tipo;
+            this.color = color;
+            this.cilindrada = cilindrada;
+            this.numeroDeSerie = numeroDeSerie;
+            this.canEjes = canEjes;
+            this.andando = andando;
+            this.bocina = bocina;
+
+        }
+
+        public bool EstaAndando()
+        {
+            return andando;
+        }
+
+        public bool EmitirSonidoBocina()
+        {
+            return bocina;
+        }
+        public bool TieneEjes(int ejes)
+        {
+            return ejes == 4;
+            //return ejes == canEjes;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Classe_Carro;
+using System.Runtime.CompilerServices;
+
+Carro carro = new Carro("z3","bmw",2020,"sedan","azul",2000,201325,2,true,true);
+
+if (carro.EmitirSonidoBocina())
+{
+    Console.WriteLine("La bocina esta sonando");
+}
+else
+{
+    Console.WriteLine("La bocina no esta sonando");
+}
+
+if (carro.TieneEjes(4))
+{
+    Console.WriteLine("tiene 4 ejes");
+}
+else
+{
+    Console.WriteLine("no tiene 4 ejes");
+}
+
+if (carro.EstaAndando())
+{
+    Console.WriteLine("El auto esta en marcha");
+}
+else
+{
+    Console.WriteLine("El auto esta parado");
+}
